adf1973a9446a6b7426dacd2bc11a67cbb68f32a
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-06-03 20:03:20 +0200

    Updates

---
+++ precache-manifest.dbecc25edc5de341cf5b504087ed48b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd21ce53f98bf48234e7008e7d92f50",
+    "url": "/react-budget-calculator/index.html"
+  },
+  {
+    "revision": "60e31e8cc92d75ea733c",
+    "url": "/react-budget-calculator/static/css/main.aca86320.chunk.css"
+  },
+  {
+    "revision": "e136d41b041e6cb271b1",
+    "url": "/react-budget-calculator/static/js/2.6bc45dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-calculator/static/js/2.6bc45dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e31e8cc92d75ea733c",
+    "url": "/react-budget-calculator/static/js/main.2c2a7eb9.chunk.js"
+  },
+  {
+    "revision": "0d1d6c000100d32a51b0",
+    "url": "/react-budget-calculator/static/js/runtime-main.26ffb01b.js"
+  }
+]);
